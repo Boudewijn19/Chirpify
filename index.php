fa6a96d8fdd3702cc145cede9ed5fa36b4edf675
Boudewijn <9025649@student.zadkine.nl>
2025-02-18 14:31:40 +0100

    Nog een test

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+require = 'db.php'
+</body>
+</html>
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";Microsoft.QuickAction.Bluetooth
+$dbname = "geekdata";
+
+try {
+    $job = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+    echo "Succesvol verbonden met de database";
+}
+catch (PDOException $e) {
+    die("Verbinding mislukt: " . $e->getMessage());
+}
+?>
